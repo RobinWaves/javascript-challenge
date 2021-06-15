a153d3ba9482b09350d3a85e7364175e88e56488
Robin Christoffersen <robin.christoffersen@gmail.com>
2021-06-14 17:34:12 -0700

    Working on table

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,17 @@
+// from data.js
+var tableData = data;
+
+// Append a table to index.html add new rows of data for each ufo sighting
+// Column for date/time, city, state, coutnry, shape and comment // Array of Dictionaries
+
+//datetime: "1/1/2010",
+//city: "benton",
+//state: "ar",
+//country: "us",
+//shape: "circle",
+//durationMinutes: "5 mins.",
+//comments: "4 bright green circles high in the sky going in circles then one bright green light at my front door."
+
+tableData.forEach(function(datetime) {
+    console.log(datetime);
+});
