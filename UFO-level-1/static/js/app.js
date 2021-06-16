309ed2d65ae4560c3c0b2c2f64c2b43a6effaecf
Robin Christoffersen <robin.christoffersen@gmail.com>
2021-06-16 08:42:48 -0700

    Updated table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,6 +1,10 @@
 // from data.js
 var tableData = data;
 
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+console.log(tableData);
 // Append a table to index.html add new rows of data for each ufo sighting
 // Column for date/time, city, state, coutnry, shape and comment // Array of Dictionaries
 
@@ -12,7 +16,10 @@ var tableData = data;
 //durationMinutes: "5 mins.",
 //comments: "4 bright green circles high in the sky going in circles then one bright green light at my front door."
 
-tableData.forEach(sighting => { var row = tbody.append("tr");
-    Object.entries(sighting).forEach([key, value]);
-    row.append("td").text(value);
+data.forEach(sighting => {
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
 });
